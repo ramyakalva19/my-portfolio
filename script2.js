5941db676f58257a25f46f5b43dc5ce0982f26ab
ramyakalva-19 <ramyakalva19@gmail.com>
2026-06-06 10:25:12 +0530

    first commit message

---
+++ script2.js
@@ -0,0 +1,12 @@
+// script.js
+
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+  themeToggle.textContent = body.classList.contains('dark-mode') ? '☀️' : '🌙';
+});
+
+
+  
